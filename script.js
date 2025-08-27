d186859f6a2eba5a6571d79a56076cdea5167750
Ashik756 <ashikurrahaman9988@gmail.com>
2025-08-27 18:06:42 +0600

    Card functionality

---
+++ script.js
@@ -0,0 +1,33 @@
+// "heart" ক্লাসযুক্ত সব এলিমেন্টকে সিলেক্ট করা হলো
+const hearts = document.getElementsByClassName("heart");
+
+// প্রতিটি এলিমেন্টের উপর লুপ চালিয়ে লিসেনার যোগ করা হচ্ছে
+for (let i = 0; i < hearts.length; i++) {
+    hearts[i].addEventListener("click", function () {
+        // ভেতরের কোড ঠিক আছে
+        const heartCount = parseInt(document.getElementById("heartCount").innerText);
+        const totalHeartCount = heartCount + 1;
+        document.getElementById("heartCount").innerText = totalHeartCount;
+    });
+}
+
+
+const naEmNum = document.getElementById("naEmNum").innerText;
+document.getElementById('btnNaEm').addEventListener("click", function () {
+    navigator.clipboard.writeText(naEmNum);
+    const copy = parseInt(document.getElementById("copyCount").innerText);
+    console.log(copy);
+    alert("নাম্বার কপি হয়েছে, 999");
+    const newCopy = copy + 1;
+    document.getElementById("copyCount").innerText = newCopy;
+})
+document.getElementById("btnNaEmCall").addEventListener("click", function () {
+    const naEmCall = parseInt(document.getElementById("coinCount").innerText);
+    if (naEmCall < 20) {
+        alert("আপনার পর্যাপ্ত কয়েন নেই কল করতে কমপক্ষে ২০ কয়েন লাগবে");
+        return;
+    }
+    const coinCount = naEmCall - 20;
+    document.getElementById("coinCount").innerText = coinCount;
+    alert("📞 Calling National Emergency, 999");
+})
